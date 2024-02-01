42ad8d23acfae8a781bc652e89065808a36dd6dc
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-01-31 23:28:27 -0800

    Install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: {
+        files: ["*.html", "./src/**/*.rs", "*.scss"],
+    },
+    darkMode: "class",
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
